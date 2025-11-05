4194ad5fb1944e8cafbbafbda41cbda3c517c6ab
Robert Michael Watson <rmwatson589@gmail.com>
2025-11-05 12:31:02 +0100

    added converted components from jss into content sdk and applied styles

--- .sitecore/component-map.ts
+++ .sitecore/component-map.ts
@@ -5,8 +5,13 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 
 // Components imported from the app itself
 import * as Demo2GeneratedTemplate from 'src/components/Demo2GeneratedTemplate';
+import * as ProductExample from 'src/components/demo-4/ProductExample';
 import * as RelatedBlogs from 'src/components/demo-3/RelatedBlogs';
 import * as GraphQlDemo from 'src/components/demo-1/GraphQlDemo';
+import * as RichText from 'src/components/converted-from-jss/RichText';
+import * as PartialDesignDynamicPlaceholder from 'src/components/converted-from-jss/PartialDesignDynamicPlaceholder';
+import * as Navigation from 'src/components/converted-from-jss/Navigation';
+import * as Container from 'src/components/converted-from-jss/Container';
 
 
 // Components must be registered within the map to match the string key with component name in Sitecore
@@ -15,8 +20,13 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
   ['Demo2GeneratedTemplate', Demo2GeneratedTemplate],
+  ['ProductExample', ProductExample],
   ['RelatedBlogs', RelatedBlogs],
   ['GraphQlDemo', GraphQlDemo],
+  ['RichText', RichText],
+  ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
+  ['Navigation', Navigation],
+  ['Container', Container],
 ]);
 
 export default componentMap;
